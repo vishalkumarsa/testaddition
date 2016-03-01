dac369d30cf568d6af2d68bf5966ab08d98faed9
vkumarsa <vkumarsa@netapp.com>
2016-03-01 14:23:11 +0530

    Commit number 1

---
+++ src/testaddition/Main.java
@@ -0,0 +1,14 @@
+package testaddition;
+
+public class Main {
+		public static void main(String args[])
+	{
+			String test = "Hello World"	;
+			int num = 5;
+			int num2= 10;
+			int x= num + num2;
+			System.out.println(test);
+			System.out.println(x);
+		
+	}
+}
